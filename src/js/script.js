9eb7a04d59db0e3e470c31a3dd6ab7ec1a4d82e5
Sergey <chubssand@gmail.com>
2023-01-17 13:39:25 +0200

    script added

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,17 @@
-var hamburger = document.querySelector.hamburger
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu_items'),
+        menuItem = document.querySelectorAll('.menu_item'),
+        hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_items_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_items_active');
+        })
+    })
+})
